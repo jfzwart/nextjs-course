b3710a547e18b54e5073ab13ba818d4cf8c6bf08
Jeroen Zwart <jeroen.zwart@live.com>
2021-05-28 11:39:03 +0200

    completed first project

---
+++ react-course/src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo title="Hello"/>
+      <Todo title="Master React"/>
+      <Todo title="Third Modal"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-course/src/components/Modal.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Modal = (props) => {
+
+    const cancelHandler = () => {
+        props.onConfirm();
+    }
+
+    const confirmHandler = () => {
+        props.onCancel();
+    }
+
+    return (
+        <div className="modal">
+            <p>Are you sure?</p>
+            <button className="btn btn--alt" onClick={cancelHandler}>Cancel</button>
+            <button className="btn" onClick={confirmHandler}>Confirm</button> 
+        </div>
+    );
+};
+
+export default Modal;
