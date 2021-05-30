e9ccd0031f9a4afa004ddaecb8f532090666c410
Jeroen Zwart <jeroen.zwart@live.com>
2021-05-30 15:57:06 +0200

    added fetch and post request with firebase backend

--- react-course-routing/src/pages/NewMeetups.js
+++ react-course-routing/src/pages/NewMeetups.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React from "react";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useHistory } from 'react-router-dom';
 
 const NewMeetups = () => {
+    const history = useHistory();
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+        "https://react-course-routing-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+            {
+                method: "POST",
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(() => {
+            history.replace('/');
+        });
+    }
+
     return (
-        <div>
-            NewMeetups
-        </div>
+        <section>
+        <h1>Add New Meetup</h1>
+        <NewMeetupForm onAddMeetup={addMeetupHandler} />
+        </section>
     );
 };
 
