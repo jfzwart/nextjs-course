9db88893527f5f11ef80680a50b2880a36759d88
Jeroen Zwart <jeroen.zwart@live.com>
2021-05-30 22:37:10 +0200

    added remaining routes and 404 page

--- nextjs-course-first/pages/clients/[id]/index.js
+++ nextjs-course-first/pages/clients/[id]/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import { useRouter } from 'next/router'
+import React from "react";
+import { useRouter } from "next/router";
+
 
 const ClientProjectPage = () => {
+    const router = useRouter();
 
-    const router = useRouter(); 
+    function loadProjectHandler() {
+         // do some things
+         // navigate away
+         router.push('/clients/max/projecta')
+    }
+
     return (
         <div>
-            
+        <h1>The Client Project Page</h1>
+        <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     );
 };
--- nextjs-course-first/pages/clients/index.js
+++ nextjs-course-first/pages/clients/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
 
 const ClientsPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const clients = [
+    { id: "max", name: "Maximilian " },
+    { id: "manu", name: "Manuel" },
+  ];
+
+  return (
+    <div>
+      <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => {
+          return (
+            <li key={client.id}>
+              <Link href={{
+                  pathname: '/clients/[id]',
+                  query: { id: client.id }
+              }}>{client.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default ClientsPage;
