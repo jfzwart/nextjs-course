8953822975cb7c24b31e02fe14742a4fa07c8690
Jeroen Zwart <jeroen.zwart@live.com>
2021-06-23 12:47:40 +0200

    added buttons and icons to events

---
+++ nextjs-course-code/components/ui/button.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import classes from "./button.module.css";
+
+function Button(props) {
+	return (
+		<Link href={props.link}>
+			<a className={classes.btn}>{props.children}</a>
+		</Link>
+	);
+}
+
+export default Button;
+
+//props.children will give the text
+//props.link will give the link
